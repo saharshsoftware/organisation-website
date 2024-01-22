c4af8f86c41e2cc15ed3decb82a53fdc03201688
Developer1@Saharsh@123 <developer1.saharsh.software@gmail.com>
2024-01-22 18:40:00 +0530

    ui-improvments

--- src/components/molecules/ProjectImage.vue
+++ src/components/molecules/ProjectImage.vue
@@ -92,24 +92,30 @@ function openLink(url: string) {
         :key="index"
       >
         <div
-          class="rounded-[30px] flex lg:flex-row flex-col items-center justify-between shrink-0 w-full relative gap-4 animate__animated"
+          class="rounded-[30px] flex lg:flex-row flex-col items-center justify-between shrink-0 w-full relative gap-4 mb-4"
         >
           <div
-            class="text-[#0e163d] text-center text-3xl leading-6 heading font-normal relative animate__animated"
+            class="text-[#0e163d] text-center text-3xl leading-6 heading font-normal relative"
           >
             {{ item?.project_name }}
           </div>
           <div
+            v-if='item?.project_link'
             class="border-solid border-[#237ac4] border-2 px-8 py-4 flex flex-row gap-2.5 items-center justify-center shrink-0 relative overflow-hidden cursor-pointer"
             @click="() => openLink(item?.project_link)"
           >
             <div
-              class="text-[#0e163d] text-center text-base leading-[22px] font-bold relative"
+              class="text-[#0e163d] text-center text-base leading-[22px] font-bold relative break-all"
             >
               {{ item?.project_link }}
             </div>
           </div>
         </div>
+        <div
+            v-if="item?.project_desc"
+            class="text-[#6e6e6e] text-left text-base leading-[30px] font-normal relative self-stretch blog-json-class"
+            v-html="renderMarkdown(item?.project_desc)"
+          ></div>
         <swiper
           :modules="modules"
           :loop="true"
@@ -126,19 +132,15 @@ function openLink(url: string) {
                   class="mt-6 flex items-center justify-center space-x-3"
                 ></figcaption>
                 <img
-                  class="w-full"
-                  alt="profile picture"
+                  class="bg-contain mx-auto border border-[#e3e3e3] shadow-md max-h-[70vh]"
+                  alt="profile picture "
                   :src="p_image.attributes.url"
                 />
               </figure>
             </div>
           </swiper-slide>
         </swiper>
-        <div
-          v-if="item?.project_desc"
-          class="text-[#6e6e6e] text-left text-base leading-[30px] font-normal relative self-stretch blog-json-class"
-          v-html="renderMarkdown(item?.project_desc)"
-        ></div>
+        
       </div>
     </section>
   </template>
