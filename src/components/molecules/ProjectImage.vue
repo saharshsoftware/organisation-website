9c663ef64f18fd583006f4681abd8821e67bb05d
Developer1@Saharsh@123 <developer1.saharsh.software@gmail.com>
2024-01-30 11:23:29 +0530

    project-image-ui-improvements

--- src/components/molecules/ProjectImage.vue
+++ src/components/molecules/ProjectImage.vue
@@ -14,9 +14,7 @@ import { STRINGS } from "../../shared/constants";
 // components
 import BreadCrumbs from "../atoms/BreadCrumbs.vue";
 import Loader from "../atoms/Loader.vue";
-import DisplayProjectInfo from "../atoms/DisplayProjectInfo.vue";
-
-
+import CustomTable from "../atoms/CustomTable.vue";
 
 const { data: aboutData, isLoading } = useQuery({
   queryKey: ["project_image"],
@@ -66,6 +64,10 @@ const breadcrumbs = [
   { label: STRINGS.PROJECTS, link: ROUTE_CONSTANTS.PROJECTS },
 ];
 
+function openLink(url: string) {
+  window.open(url, "_blank");
+}
+
 </script>
 <template>
   <section class="common-padding py-9 relative bg-primary-color">
@@ -78,11 +80,32 @@ const breadcrumbs = [
     <Loader />
   </template>
   <template v-if="formattedProjectImage">
-    <section ref="el" class="flex flex-col common-padding gap-8 py-4">
+    <!-- <section ref="el" class="flex flex-col common-padding gap-8 py-4">
       <div v-for="(item, index) in formattedProjectImage?.client_projects" :key="index">
         <DisplayProjectInfo :item="item" :isDesktop="isDesktop" :isTablet="isTablet" />
       </div>
+    </section> -->
+    <section ref="el" class="flex flex-col common-padding gap-8 py-4">
+
+    <CustomTable
+      :tableHeader="['Project', 'Client', 'Live URL']"
+      :tableData="[...formattedProjectImage?.client_projects]"
+    >
+      <!-- Custom template for tbody rows -->
+      <template v-slot:tbodyRow="{ row }">
+        <tr>
+          <td class="common-td">{{ row.project_name }}</td>
+          <td class="common-td" >{{ row.client }}</td>
+          <td class="link link-primary common-td" @click="() => openLink(row?.project_link)"> Link</td>
+          <!-- <td>{{ row.location }}</td>
+          <td>{{ row.lastLogin }}</td>
+          <td>{{ row.favoriteColor }}</td> -->
+          <!-- Add more customizations as needed -->
+        </tr>
+      </template>
+    </CustomTable>
     </section>
+    
   </template>
 </template>
 
